488804ddfb07142d6bf50b6692b3968b18b23f01
yongk7614-creator <yongk7614@gmail.com>
2026-04-14 21:18:30 +0900

    Update mirobot_moveit_tracker.launch.py

--- mirobot_moveit_tracker/launch/mirobot_moveit_tracker.launch.py
+++ mirobot_moveit_tracker/launch/mirobot_moveit_tracker.launch.py
@@ -1,1 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="mirobot_moveit_tracker",
+                executable="wheel_stop_to_goal_node",
+                name="wheel_stop_to_goal_node",
+                output="screen",
+            ),
+            Node(
+                package="mirobot_moveit_tracker",
+                executable="moveit_goal_node",
+                name="moveit_goal_node",
+                output="screen",
+            ),
+        ]
+    )

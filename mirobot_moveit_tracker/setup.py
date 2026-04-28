b258faf29484182742cc1ce64d886e7a31cc4829
yongk7614-creator <yongk7614@gmail.com>
2026-04-28 17:29:45 +0900

    Update setup.py

--- mirobot_moveit_tracker/setup.py
+++ mirobot_moveit_tracker/setup.py
@@ -9,7 +9,13 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        ("share/" + package_name + "/launch", ["launch/mirobot_moveit_tracker.launch.py"]),
+        (
+            "share/" + package_name + "/launch",
+            [
+                "launch/mirobot_moveit_tracker.launch.py",
+                "launch/mirobot_wlkata_system.launch.py",
+            ],
+        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,

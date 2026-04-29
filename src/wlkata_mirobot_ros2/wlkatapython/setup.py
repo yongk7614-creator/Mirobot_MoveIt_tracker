6eabd55d33b38457b3592a06a40bc967d2cdfea6
yongk7614-creator <yongk7614@gmail.com>
2026-04-29 21:17:16 +0900

    Update setup.py

--- src/wlkata_mirobot_ros2/wlkatapython/setup.py
+++ src/wlkata_mirobot_ros2/wlkatapython/setup.py
@@ -1,23 +1,25 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
+
+package_name = "wlkatapython"
 
 setup(
-    name='wlkatapython', # 自定义包名 #Custom package name
-    version='0.1.1', # 包的版本号 #Package version number
-    description='WLKATA-Mirobot/E4/MT4/Haro380/MS4220 Multiple robotic arm control!', # 描述信息 #Description information
-    long_description=open('README.md').read(),  # 长描述，通常是README #Long description, usually README
-    long_description_content_type='text/markdown',  # 长描述的格式 #Long description format
-    author='DSS', # 作者 #Author
-    packages=find_packages(include=['*', 'tests*']),  # 自动找到项目中的所有包 #Automatically find all packages in the project
-    py_modules=[
-        'wlkatapython'
-    ] ,# 包中包含的模块 #Modules included in the package
-    install_requires=[
-        'pyserial'
+    name=package_name,
+    version="0.1.1",
+    description="WLKATA Mirobot/E4/MT4/Haro380/MS4220 Python serial SDK.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="DSS",
+    packages=find_packages(include=["*", "tests*"]),
+    py_modules=["wlkatapython"],
+    install_requires=["pyserial"],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ]
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
